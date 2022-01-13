0c9bce1598397a291ec86c9ab2f8f8698a8bbc78
kolatham <92490383+kolatham@users.noreply.github.com>
2022-01-13 14:51:11 -0500

    did engineer test

---
+++ tests/Engineer.test.js
@@ -0,0 +1,23 @@
+
+const Engineer = require('../lib/Engineer');
+
+
+test('creates an Engineer object', () =>{
+    const engineer = new Engineer('Alec', 2, 'alec@fakemail.com', 'ibealec');
+
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+
+
+test('gets engineer github value', () =>{
+    const engineer = new Engineer('Alec', 2, 'alec@fakemail.com', 'ibealec');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+
+test('gets role of employee',() => {
+    const engineer = new Engineer('Alec', 2, 'alec@fakemail.com', 'ibealec');
+    
+    expect(engineer.getRole()).toEqual("Engineer");
+});
